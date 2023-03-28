3c782e4bc2ee5d13d7b7281ac24de8381b3a40fc
Novato Pro <admin@novato.pro>
2023-03-28 14:17:58 -0500

    Push all vendor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,13 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta name="description" content="Novato pro is a tech website, developed generally for learning purposes.">
     <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="icon" type="image/jpg" href="/img/logo.jpg">
-    <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
+    
     <!-- Scripts -->
     @vite(['resources/js/app.js'])
     @vite(['resources/css/app.css'])
