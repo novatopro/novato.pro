de343831ce420313482649acc47db4ef7ff70c43
Jhordy Barrera <jbarreraballestas@correo.unicordoba.edu.co>
2023-06-24 12:06:35 -0500

    Remove description and update laravel-lang/lang

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,6 @@
     
     <div class="container">
     <x-validation-errors />
-
         {{-- <script src="https://gist.github.com/jbarreraballestas/2190314ca629712baeb761fef2ebb5d1.js"></script> --}}
     </div>
     <div
@@ -10,13 +9,8 @@
         <div class="max-w-7xl mx-auto p-6 lg:p-8">
             <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
                 <div class="ml-4 text-sm text-gray-500 dark:text-gray-400 sm:ml-0">
-                    <h2>Acerca de Novato.pro</h2>
-                    <p>Novato.pro es un sitio web en crecimiento que tiene como objetivo brindar un espacio para aprender y enseñar habilidades diversas. El sitio se centra en reunir a personas novatas y expertas en diferentes áreas del conocimiento, fomentando la colaboración y el intercambio de conocimientos.</p>
-<p>En Novato.pro, todos somos novatos y pros, lo que significa que todos tenemos algo que aprender y algo que enseñar. No importa cuál sea tu nivel de experiencia, puedes encontrar oportunidades para mejorar tus habilidades y compartir tus conocimientos con otros.</p>
-<p>El sitio ofrece una variedad de recursos y herramientas para facilitar el aprendizaje y la enseñanza. Puedes acceder a tutoriales, cursos en línea, artículos informativos y foros de discusión, donde puedes interactuar con expertos y otros usuarios con intereses similares.</p>
-<p>Novato.pro se destaca por su enfoque inclusivo y amigable. No importa cuál sea tu área de interés, encontrarás comunidades y grupos dedicados a diferentes temas, desde programación y diseño hasta música y arte. Además, el sitio está constantemente en crecimiento, por lo que siempre hay nuevas oportunidades y contenidos para explorar.</p>
+                    <h2>Bienvenido a Novato.pro</h2>
                 </div>
-
             </div>
         </div>
     </div>
